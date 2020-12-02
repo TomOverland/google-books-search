5040f34aa792fac0ce19ba51cb8d892f0484d28e
Tom Overland <tomoverland03@gmail.com>
2020-12-02 10:34:43 -0600

    set up server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000
 const app = express();
@@ -12,3 +13,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+// Defining API routes
+app.use(routes);
+
+// Connect to the Mongo Database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+
+// Listen call to host app locally
+app.listen(PORT, () => {
+    console.log(`Server now listening on port ${PORT}.`)
+})
