fb6e8a562a3471564d117de434a2e20f756371fe
Tom Overland <tomoverland03@gmail.com>
2020-12-02 12:32:51 -0600

    created controllers for db

--- controllers/booksControllers.js
+++ controllers/booksControllers.js
@@ -3,7 +3,7 @@ const db = require('../models');
 module.exports = {
     findAll: (req, res) => {
         db.Book.find(req.query).sort({ date: -1 }).then(dbModel => {
-            res.json(dbModel)
+            res.json(dbModel);
         }).catch(error => {
             console.log(error);
             res.status(422).json(error);
@@ -14,5 +14,31 @@ module.exports = {
             console.log(error);
             res.status(422).json(error);
         });
+    },
+    create: (req, res) => {
+        db.Book.create(req.body).then(dbModel => {
+            res.json(dbModel);
+        }).catch(error => {
+            console.log(error);
+            res.status(422).json(error);
+        });
+    },
+    update: (req, res) => {
+        db.Book.findOneAndUpdate({ _id: req.params.id }, req.body).then(dbModel => {
+            res.json(dbModel)
+        }).catch(error => {
+            console.log(error);
+            res.status(422).json(error);
+        });
+    },
+    remove: (req, res) {
+        db.Book.findById({ _id: req.params.id }).then(dbModel => {
+            dbModel.remove()
+        }).then(dbModel => {
+            res.json(dbModel);
+        }).catch(error => {
+            console.log(error);
+            res.status(422).json(error);
+        });
     }
-}
+};
