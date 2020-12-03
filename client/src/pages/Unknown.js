52a786b6cc695de820b0caa7a6eb61a508b4457b
Tom Overland <tomoverland03@gmail.com>
2020-12-03 15:09:59 -0600

    added 404 page

---
+++ client/src/pages/Unknown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../components/Header/Header";
+
+function Unknown() {
+    return (
+        <div>
+            <Header>
+                <h1>404 Page Not Found</h1>
+                <p>Please check your spelling and try again!</p>
+            </Header>
+        </div>
+    )
+}
+
+export default Unknown;
