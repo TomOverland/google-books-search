7a65e073067a128b0c125fd9f460bb6b65f8b6ab
Tom Overland <tomoverland03@gmail.com>
2020-12-03 14:32:28 -0600

    trying to get react page to load root

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,18 +5,12 @@ export default {
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
     },
     deleteBook: id => {
-        return axios.delete(`api/books/${id}`).then(result => {
-            result.data;
-        })
+        return axios.delete(`api/books/${id}`).then(result => result.data)
     },
     saveBook: bookData => {
-        return axios.post("/api/books", bookData).then(result => {
-            result.data;
-        });
+        return axios.post("/api/books", bookData).then(result => result.data);
     },
     savedBooks: () => {
-        return axios.get("/api/books").then(result => {
-            result.data;
-        })
+        return axios.get("/api/books").then(result => result.data)
     }
 };
