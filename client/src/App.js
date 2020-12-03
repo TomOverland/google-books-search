4e77cab83d4c65b71af514dbcb07daacd0f916fb
Tom Overland <tomoverland03@gmail.com>
2020-12-03 12:25:25 -0600

    created NavBar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import Header from "./components/Header/Header";
 import Wrapper from "./components/Wrapper";
 
