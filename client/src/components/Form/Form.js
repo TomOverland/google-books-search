8c57f019d6b9c12d1b563083ae78b3c1064d43b8
Tom Overland <tomoverland03@gmail.com>
2021-01-10 22:57:20 -0600

    adjusted styling for search for books input

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,25 +4,29 @@ function Form(props) {
   return (
     <div className="container">
       <form>
-        <div className="form-group text-center">
+        <div className="form-group text-center pt-4">
           <label htmlFor="search">
             <h2>Search for Books</h2>
           </label>
-          <input
-            onChange={props.handleInputChange}
-            value={props.search}
-            name="search"
-            type="text"
-            className="formControl ml-2"
-            placeholder="Search"
-            id="search"
-          />
-          <button
-            onClick={props.handleFormSubmit}
-            className="btn btn-secondary ml-2"
-          >
-            Search
-          </button>
+          <div className="input-group row justify-content-center">
+            <input
+              onChange={props.handleInputChange}
+              value={props.search}
+              name="search"
+              type="text"
+              className="form-control ml-2 col-sm-4"
+              placeholder="Search"
+              id="search"
+            />
+            <div className="input-group-append">
+              <button
+                onClick={props.handleFormSubmit}
+                className="btn btn-secondary ml-2"
+              >
+                Search
+              </button>
+            </div>
+          </div>
         </div>
       </form>
     </div>
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 function Header() {
-    return (
-            <div className="jumbotron text-center">
-                <div className="container">
-                    <h1>Google Books Search</h1>
-                </div>
-            </div>
-        
-    )
+  return (
+    <div className="jumbotron text-center">
+      <div className="container">
+        <h1>Google Books Search</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
