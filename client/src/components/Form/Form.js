c2573111b66345362802538c80267584bc94446e
Tom Overland <tomoverland03@gmail.com>
2021-01-10 23:17:17 -0600

    reverting to old working input

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,27 +4,25 @@ function Form(props) {
   return (
     <div className="container">
       <form>
-        <div className="form-group text-center pt-4">
+        <div className="form-group text-center">
           <label htmlFor="search">
             <h2>Search for Books</h2>
           </label>
-          <div className="row justify-content-center">
-            <input
-              onChange={props.handleInputChange}
-              value={props.search}
-              name="search"
-              type="text"
-              className="form-control col-sm-4 ml-2 "
-              placeholder="Search"
-              id="search"
-            />
-            <button
-              onClick={props.handleFormSubmit}
-              className="btn btn-secondary ml-2"
-            >
-              Search
-            </button>
-          </div>
+          <input
+            onChange={props.handleInputChange}
+            value={props.search}
+            name="search"
+            type="text"
+            className="formControl ml-2"
+            placeholder="Search"
+            id="search"
+          />
+          <button
+            onClick={props.handleFormSubmit}
+            className="btn btn-secondary ml-2"
+          >
+            Search
+          </button>
         </div>
       </form>
     </div>
